090091065711fa52e134660b6b1a9ca850a8e648
troygamurot <troygamurot@csu.fullerton.edu>
2021-02-25 22:54:04 -0700

    Simple.c working with printing HZ and jiffies

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # cpsc351_project1_jiffies
  
+simple.c has the jiffies, HZ, gcd, and Golden Prime Ratio prints functioning.
+
--- simple/simple.c
+++ simple/simple.c
@@ -14,19 +14,24 @@
 #include <linux/kernel.h>
 #include <linux/hash.h>
 #include <linux/gcd.h> 
+#include <asm/param.h>
+#include <linux/jiffies.h>
 
 /* This function is called when the module is loaded. */
 int simple_init(void)
 {
        printk(KERN_INFO "Loading Module\n");
-       printk(KERN_INFO "Golden Ratio Prime is: %lu n", GOLDEN_RATIO_PRIME);
+       printk(KERN_INFO "Golden Ratio Prime is: %lu\n", GOLDEN_RATIO_PRIME);
+       printk(KERN_INFO "Jiffies in simple_init() is: %lu\n", jiffies);
+       printk(KERN_INFO "HZ in simple_init() is: %lu\n", HZ);
 
        return 0;
 }
 
 /* This function is called when the module is removed. */
 void simple_exit(void) {
-       printk(KERN_INFO "GCD of 3300 and 24 is: %lu n", gcd(3300,24));
+       printk(KERN_INFO "GCD of 3300 and 24 is: %lu\n", gcd(3300,24));
+       printk(KERN_INFO "Jiffies in simple_exit() is: %lu\n", jiffies);
 	printk(KERN_INFO "Removing Module\n");
 }
 
